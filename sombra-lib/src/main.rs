e52044a14258bccd6b71a13615b6b03ea1dabbfe
Atilogit <yannickAlexander@gmx.de>
2023-02-24 10:03:28 +0100

    Run tests in parallel

--- sombra-lib/src/main.rs
+++ sombra-lib/src/main.rs
@@ -1,16 +1,15 @@
+use futures::future::join_all;
 use sombra_lib::{Battletag, Client};
 
 #[tokio::main]
 async fn main() {
     let client = Client::new();
-    let profile = client
-        .profile(&Battletag::new("gazanie", 2915))
-        .await
-        .unwrap();
-    dbg!(profile);
-    let profile = client
-        .profile(&Battletag::new("Zusor", 2553))
-        .await
-        .unwrap();
-    dbg!(profile);
+
+    let profiles = join_all([
+        client.profile(&Battletag::new("gazanie", 2915)),
+        client.profile(&Battletag::new("Zusor", 2553)),
+    ])
+    .await;
+
+    dbg!(profiles);
 }
